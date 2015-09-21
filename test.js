1896519cb26a152fc929030c0500e7c998766cfc
Kyle Mitchell <kyle@kemitchell.com>
2015-09-20 22:46:34 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,7 @@
+var tv4 = require('tv4')
+var assert = require('assert')
+var schema = require('.')
+
+require('./examples.json')
+  .forEach(function(example) {
+    assert(tv4.validate(example, schema)) })
